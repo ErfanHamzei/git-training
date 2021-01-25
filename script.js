68237d6c009942f08e9345bcfdc3241250c58578
erfanhamzei <ehamzei40@gmail.com>
2021-01-25 22:11:48 +0330

    dblclick event added

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const card = document.querySelector(".card");
 card.addEventListener("click",() => {
 alert("You clicked the card !");
 });
+
+
+card.addEventListener("dblclick",() => {
+alert("You Double Clicked the card");
+});
