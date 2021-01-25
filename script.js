3dbbc2a4640e969b15bba67c25eb72a12ac85f57
erfanhamzei <ehamzei40@gmail.com>
2021-01-25 22:12:55 +0330

    Double Click added

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const card = document.querySelector(".card");
 card.addEventListener("click",() => {
 alert("You clicked the card !");
 });
+
+card.addEventListener("dblclick",() => {
+    alert("Double Click !");
+})
